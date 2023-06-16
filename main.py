9c6fab4e20c3616e4ca8bbec8bb1a092a2d85c97
spae-roh <jatinchaudhary4444@gmail.com>
2023-06-16 22:29:30 +0530

    add items functionality enhancement vending machine

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 if __name__ == "__main__":
     class Vending_machine:
+        '''vending machine code'''
+
         def __init__(self, items: int, money: int) -> None:
             '''constructor items-> total items in vending machine money-> buying price of each item'''
             self.items = items
             self.money = money
 
         def add_items(self, items):
-            self.items += items
+            if self.items+items <= 100:
+                self.items += items
+            else:
+                print("request container of items cannot be added. limited space left is ",(100-self.items))
 
 
 
